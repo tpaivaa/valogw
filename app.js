be35a9b05bb146dfc6d064b3d26902c93f6f3a01
Tomi Hakkarainen <tomi.hakkarainen@advania.com>
2022-05-15 20:01:37 +0300

    data to string

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ const serialport = require('./serial')
 
 // Switches the port into "flowing mode"
 serialport.on('data', function (data) {
-  console.log('Data:', data)
+  console.log('Data:', data.toString())
 })
