cb5602414dd6995d2638a1a004ffd5976a02b977
Tomi Hakkarainen <tomi.hakkarainen@advania.com>
2022-05-15 19:51:44 +0300

    serial

---
+++ app.js
@@ -0,0 +1,7 @@
+const serialport = require(serial)
+
+
+// Switches the port into "flowing mode"
+serialport.on('data', function (data) {
+  console.log('Data:', data)
+})
---
+++ serial.js
@@ -0,0 +1,23 @@
+const { SerialPort } = require('serialport')
+const port = new SerialPort({
+  path: '/dev/ttyUSB1',
+  baudRate: 115200,
+  autoOpen: true,
+})
+
+port.open(function (err) {
+  if (err) {
+    return console.log('Error opening port: ', err.message)
+  }
+
+  // Because there's no callback to write, write errors will be emitted on the port:
+  // port.write('main screen turn on')
+})
+
+// The open event is always emitted
+port.on('open', function() {
+  // open logic
+  console.log('Serial port is open')
+})
+
+export default port
