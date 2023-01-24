5ff3074957e79e15128a288b09ec4737e8db3ca8
Tomi Hakkarainen <Tomi.Hakkarainen@enreach.com>
2023-01-24 21:42:06 +0200

    lets play with typescript

--- app.js
+++
@@ -1,7 +0,0 @@
-const { port, parser } = require('./serial')
-
-
-// Switches the port into "flowing mode"
-parser.on('data', function (data) {
-  console.log('Data:', data.toString())
-})
---
+++ build/app.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const serial_1 = require("../build/serial");
+const helpers_1 = require("./helpers");
+// Switches the port into "flowing mode"
+serial_1.parser.on('data', function (data) {
+    try {
+        const input = JSON.parse(data);
+        (0, helpers_1.handleInput)(input);
+    }
+    catch (e) {
+        console.log(data.toString());
+        serial_1.port.write((0, helpers_1.switchState)(helpers_1.Switches.ykmh1, helpers_1.States.query));
+    }
+});
