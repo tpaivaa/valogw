1954830598de2632cafc8bec4db9eecc19e771bd
Tomi Hakkarainen <Tomi.Hakkarainen@enreach.com>
2023-01-27 20:11:15 +0200

    mqtt

--- build/mqtt.js
+++ build/mqtt.js
@@ -2,7 +2,7 @@
 const mqtt = require('mqtt');
 require('dotenv').config({ path: '../.env', override: true });
 const clientId = `mqtt_valogw${Math.random().toString(16).slice(3)}`;
-const connectUrl = process.env.MQTT_BROKER_URL;
+const connectUrl = process.env.MQTT_BROKER_URL || '';
 const client = mqtt.connect(connectUrl, {
     clientId,
     clean: true,
@@ -10,21 +10,5 @@ const client = mqtt.connect(connectUrl, {
     username: process.env.MQTT_USER,
     password: process.env.MQTT_PASS,
     reconnectPeriod: 1000,
-});
-const sub_topic = 'stat/light/#';
-const pub_topic = 'cmnd/light/veranta/POWER';
-client.on('connect', () => {
-    console.log('Connected');
-    client.subscribe([sub_topic], () => {
-        console.log(`Subscribe to topic '${sub_topic}'`);
-    });
-    client.publish(pub_topic, 'ON', { qos: 1, retain: false }, (error) => {
-        if (error) {
-            console.error(error);
-        }
-    });
-});
-client.on('message', (topic, payload) => {
-    console.log('Received Message:', topic, payload.toString());
 });
 module.exports = { client };
--- src/app.ts
+++ src/app.ts
@@ -15,3 +15,20 @@ parser.on('data', function (data: string) {
     // port.write(switchState(Switches.ykmh1,States.query))
   }
 })
+
+const sub_topic = 'stat/light/#'
+const pub_topic = 'cmnd/light/veranta/POWER'
+client.on('connect', () => {
+  console.log('Connected')
+  client.subscribe([sub_topic], () => {
+    console.log(`Subscribe to topic '${sub_topic}'`)
+  })
+  client.publish(pub_topic, 'ON', { qos: 1, retain: false }, (error) => {
+    if (error) {
+      console.error(error)
+    }
+  })
+})
+client.on('message', (topic, payload) => {
+  console.log('Received Message:', topic, payload.toString())
+})
